177178c8bd856c9e08f498d394212e8a74b69287
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-06-11 13:49:32 -0400

    fddfsdfsdfsdfs
    deeq

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:montyhacks2022/screens/Homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  bool islogin =false;
+  final prefs = await SharedPreferences.getInstance();
+  islogin = prefs.getBool('deleteAll') ?? false;
   await Firebase.initializeApp();
+  runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/screens/login/loginScreen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../main.dart';
+
+class login extends StatefulWidget {
+  login({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _login createState() => _login();
+}
+
+class _login extends State<login> {
+  @override
+  void initState() {}
+
+  @override
+  Widget build(BuildContext context) {
+    final myController1 = TextEditingController();
+
+    return Scaffold(
+        body: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Auth Key:"),
+        Padding(
+            padding: const EdgeInsets.all(30),
+            child: TextField(
+              controller: myController1,
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Key',
+              ),
+            )),
+        ElevatedButton(
+            onPressed: () async {
+              final prefs = await SharedPreferences.getInstance();
+              //normal login
+              if (myController1.text == '69420') {
+                await prefs.setBool('deleteAll', true);
+
+                // ignore: use_build_context_synchronously
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MyApp(),
+                  ),
+                );
+              }
+              //show key wrong
+              if (myController1 == '69420') {            
+                if (myController1.text == '42069') {
+                  return;
+                }
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text("Wrong Key"),
+                  ),
+                );
+              }
+            },
+            child: const Text("Submit"))
+      ],
+    )));
+  }
+}
